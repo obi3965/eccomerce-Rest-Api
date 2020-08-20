7c6e307099abe2cf27d938e6a50aed8d78b10313
obi3965 <46780156+obi3965@users.noreply.github.com>
2020-08-20 12:28:14 +0200

    front-end folder

---
+++ front-end/admin-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Jumbotron } from 'react-bootstrap'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Jumbotron>
+        <h1>admin dashboard</h1>
+      </Jumbotron>
+    </div>
+  );
+}
+
+export default App;
